7957bbd721b729b87828da403ac506f0f05070d1
Haftom Fseha <hafmob.inc@gmail.com>
2021-12-31 15:03:38 -0600

    activity_main - land layout added

--- app/src/main/java/com/miu/geoquiz/MainActivity.kt
+++ app/src/main/java/com/miu/geoquiz/MainActivity.kt
@@ -2,41 +2,115 @@ package com.miu.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
+    private lateinit var questionTextview: TextView
 
     private lateinit var toast: Toast;
+
+    private val questionBank = listOf(
+        Question(R.string.question_text, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
+    }
+
+    private var currentIndex = 0;
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
-
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
+        questionTextview = findViewById(R.id.question_textview)
+
 
         trueButton.setOnClickListener {
             view: View ->
-            toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
-            toast.setGravity(Gravity.TOP, 0, 0)
-            toast.show()
+            checkAnswer(true)
         }
 
         falseButton.setOnClickListener {
             view: View ->
-            toast = Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT)
-            toast.setGravity(Gravity.TOP, 0, 0)
-            toast.show()
+            checkAnswer(false)
         }
 
+        nextButton.setOnClickListener {
+            view: View ->
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        prevButton.setOnClickListener {
+            view: View ->
+            if(currentIndex > 0)
+            currentIndex = (currentIndex - 1) % questionBank.size
+            updateQuestion()
+        }
+
+        questionTextview.setOnClickListener{
+            view:  View ->
+            currentIndex = (currentIndex + 1 ) % questionBank.size
+            updateQuestion()
+        }
+    }
+
+    private fun updateQuestion(){
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextview.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean){
+        val correctAnswer = questionBank[currentIndex].answer
+
+        val messageResId = if(userAnswer == correctAnswer){
+            R.string.correct_toast
+        }
+        else {
+            R.string.incorrect_toast
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
 
 }
--- app/src/main/java/com/miu/geoquiz/Question.kt
+++ app/src/main/java/com/miu/geoquiz/Question.kt
@@ -1,4 +1,5 @@
 package com.miu.geoquiz
 
-class Question {
-}
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val textResId: Int, val answer:Boolean)
